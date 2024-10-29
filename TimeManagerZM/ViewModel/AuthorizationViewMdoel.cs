ddbedfe3d40f6d416799d3e93ab4307125ad35e6
zabro <zabro@DESKTOP-OEGATFJ>
2024-10-29 01:19:48 +0100

    Design AuthorizationView

--- TimeManagerZM/ViewModel/AuthorizationViewMdoel.cs
+++ TimeManagerZM/ViewModel/AuthorizationViewMdoel.cs
@@ -3,16 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using TimeManagerZM.Services;
+using TimeManagerZM.View;
 
 namespace TimeManagerZM.ViewModel
 {
     public class AuthorizationViewMdoel : ViewModelBase
     {
         private readonly MainViewModel _mainViewModel;
+
+        // Commands
+        public ICommand RegisterCommand { get; }
+        public ICommand LoginCommand { get; }
+
+        private string _userName;
+        private string _password;
+
+        public string UserName
+        {
+            get { return _userName; }
+            set { SetProperty(ref _userName, value, nameof(UserName)); }
+        }
+
+        public string UserPassword
+        {
+            get => _password;
+            set => SetProperty(ref _password, value, nameof(UserPassword));
+        }
+
+
         public AuthorizationViewMdoel(MainViewModel mainViewModel) 
         {
             _mainViewModel = mainViewModel;
+            RegisterCommand = new MyICommand(Register);
+            LoginCommand = new MyICommand(Login);
+        }
+
+        // TODO: Дописать логику проверки пароля и реализацию кнопок
+        private void Register()
+        {
+            if (UserPassword.Length >= 8 && !String.IsNullOrEmpty(UserPassword))
+            {
+            }
+            else { }
+        }
+
+        private void Login()
+        {
+
         }
     }
 }
